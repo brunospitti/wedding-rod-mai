65805b2775e2182bae6472f91b8dc0d40558cbe2
Bruno Spitti <bruno.olivares@live.com>
2021-01-17 18:43:00 +0000

    going down

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -105,7 +105,6 @@ export const Form = (props) => {
           >
             {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
             <input type="hidden" name="form-name" value="confirmation" />
-            <input type="hidden" name="going" />
             <p hidden>
               <label>
                 Don’t fill this out:{' '}
@@ -145,6 +144,7 @@ export const Form = (props) => {
               />
             </StyledLabel>
 
+            <input hidden type="hidden" name="going" />
             <StyledButton type="submit" onClick={() => setGoing('yes')}>
               <div>{goingLabel}</div>
             </StyledButton>
