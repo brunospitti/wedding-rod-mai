14e1992e1b6fe5754966fafa87fbca2696687233
Bruno Spitti <bruno.olivares@live.com>
2021-02-01 14:01:44 +0000

    delete sections

--- src/components/sections/PresentsSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Section } from '../helpers/Section';
-import { Presents } from '../Presents';
-
-export const PresentsSection = ({ presents }) => (
-  <Section>
-    <Presents presents={presents} />
-  </Section>
-);
